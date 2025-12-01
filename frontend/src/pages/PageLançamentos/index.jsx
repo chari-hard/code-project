b88f18cbc3f2f7cfbfddde4c7af384465d20151f
ykwnico <nicolaslmendes75@gmail.com>
2025-12-01 08:59:17 -0300

    section lancamentos + camisa images

--- frontend/src/pages/PageLançamentos/index.jsx
+++ frontend/src/pages/PageLançamentos/index.jsx
@@ -3,34 +3,35 @@ import ImagemCamisa from '../../assets/images/camisa-outdated.svg'
 import ImagemCamisa2 from '../../assets/images/camisa-pain.svg'
 import ImagemCamisa3 from '../../assets/images/camisa-raveparty.svg'
 import ImagemCamisa4 from '../../assets/images/camisa-human.svg'
+import { Link } from 'react-router-dom'
 import './style.css'
 
 
 
 const listaDeRoupas = [
     {
-        imagemRoupa: { ImagemCamisa },
+        imagemRoupa:  ImagemCamisa,
         nomeRoupa: "CAMISA OUTDATED",
         descontoRoupa: "99,99",
         precoRoupa: "79,99"
     },
 
     {
-        imagemRoupa: { ImagemCamisa2 },
+        imagemRoupa:  ImagemCamisa2,
         nomeRoupa: "CAMISETA PAIN",
         descontoRoupa: "99,99",
         precoRoupa: "79,99"
     },
 
     {
-        imagemRoupa: { ImagemCamisa3 },
+        imagemRoupa:  ImagemCamisa3 ,
         nomeRoupa: "CAMISETA RAVE PARTY",
         descontoRoupa: "99,99",
         precoRoupa: "79,99"
     },
 
     {
-        imagemRoupa: { ImagemCamisa4 },
+        imagemRoupa:  ImagemCamisa4 ,
         nomeRoupa: "CAMISA HUMAN ARE GROSS",
         descontoRoupa: "99,99",
         precoRoupa: "79,99"
@@ -42,12 +43,12 @@ export const PageLancamentos = () => {
     return (
         <>
             <div className='linha-de-sessoes'>
-                <a href="">Página inicial</a>
-                <a href="">Ofertas</a>
-                <a href="">Coleção</a>
+                <Link to= "/">Página inicial</Link>
+                <Link to= "/ofertas">Ofertas</Link>
+                <Link to= "/colecao-inverno">Coleção de inverno</Link>
             </div>
 
-            <h2 className='h2-lancamentos'>Camisetas</h2>
+            <h2 className='h2-lancamentos'>Lançamentos</h2>
 
             <div className='container-section-lancamentos'>
 
