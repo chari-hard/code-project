c4fda6dcad776a8268c3cb55fb8e6e1c14d52f79
“julio” <juliocgdlima@gmail.com>
2025-12-08 09:49:01 -0300

    estrutura do backend completa

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
 
-router.get('/', adminController.listarCamisetas); // Rota para listar todas as camisetas
-router.post('/cadastrar', adminController.cadastrarCamiseta); // Rota para adicionar uma nova camiseta
-router.put('/alterar/:id', adminController.alterarCamiseta); // Rota para alterar uma camiseta existente
-router.delete('/deletar/:id', adminController.deletarCamiseta); // Rota para deletar uma camiseta existente
+router.get('/', adminController.listarProdutos);
+router.post('/cadastrar', adminController.cadastrarProduto);
+router.put('/alterar/:id', adminController.alterarProduto);
+router.delete('/deletar/:id', adminController.deletarProduto);
 
 module.exports = router;
